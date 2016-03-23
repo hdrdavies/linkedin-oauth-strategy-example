9213e773730c1a44f28d42f47efc60c96a695a0d
tormod17 <tormodsmith@gmail.com>
2016-03-23 11:45:29 +0000

    setting user to cookie

--- plugins/login.js
+++ plugins/login.js
@@ -1,40 +1,56 @@
 
-var Users = [{ name: 'Tornado', scope: 'admin', id : 'VkekfrcDpl'},
-{name:'hurricane', scope:['admin', 'super'], id:'iIkUSpzijO'}];
+
+  var users = [{ name: 'Tornado', scope: 'admin', id : 'VkekfrcDpl'},
+  {name:'hurricane', scope:['admin', 'super'], id:'iIkUSpzijO'}];
 
 
-const getValidatedUser = (request_id, users ) =>{
-/// take request from linked in user id and check against exisiting Users
+  const getValidatedUser = (request_id, users , callback ) =>{
+    /// check the user id from the request against existings users return user details.
+    /// set the user details into the cookie.
+    var user =   users.find((user) => {
+      return request_id === user.id
+    })
+    if (user) {
+      console.log('USER DETECTED',user);
 
+      callback(user);
+    } else {
+      console.log('error');
+    }
+  }
 
-}
-
-exports.register = function(server, options, next) {
+  exports.register = function(server, options, next) {
 
     server.route({
 
-        method: 'GET',
-        path: '/login',
-        config: {
-            auth: 'linkedin-oauth',
-            handler: (request, reply) => {
+      method: 'GET',
+      path: '/login',
+      config: {
+        auth: 'linkedin-oauth',
+        handler: (request, reply) => {
 
-              if (request.auth.isAuthenticated) {
+        if (request.auth.isAuthenticated) {
 
-                request.cookieAuth.set(request.auth.credentials);
+            var id = request.auth.credentials.profile.id;
 
+            getValidatedUser(id, users , (user) => {
+              request.cookieAuth.set(user);
                 return reply('Hello ' + request.auth.credentials.profile.name.first +
-                ', you\'re logged in...' +
-                'Click <a href=/logout>here</a> to logout'
+                  ', you\'re logged in...' +
+                  'Click <a href="/logout">here</a> to logout' +
+                  'Click <a href="/">here</a> to go home'
                 );
-              }
+            });
+
+        } else {
               reply('Not logged in...').code(401);
-            }
         }
-    });
-    return next();
+      }
+    }
+  });
+  return next();
 };
 
 exports.register.attributes = {
-    name: 'Login'
+  name: 'Login'
 };
